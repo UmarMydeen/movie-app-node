f5aa4c781d97d21d120cb0b17695bcd150048611
Umar Mydeen Habib Rahiman <umarmydeenhabibrahiman@umars-mbp.home>
2021-08-08 02:37:44 +0100

    route config

---
+++ api/routes/moviesRoutes.js
@@ -0,0 +1,15 @@
+var {
+    listAllMovies, createMovie, getMovieDetails
+} = require('../../controllers/moviesController');
+
+const MoviesRoutes = (app) => {
+    // Movies Routes
+    app.route('/movies')
+        .get(listAllMovies)
+        .post(createMovie);
+
+    app.route('/Movies/:id')
+        .get(getMovieDetails)
+};
+
+module.exports = MoviesRoutes;
