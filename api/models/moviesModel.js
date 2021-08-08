bdfd5e3a758e9e0fa1ac40f72920cd509d6f9a48
Umar Mydeen Habib Rahiman <umarmydeenhabibrahiman@umars-mbp.home>
2021-08-08 02:38:50 +0100

    mongo model config

---
+++ api/models/moviesModel.js
@@ -0,0 +1,26 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MoviesSchema = new Schema({
+    language: { type: String },
+    location: { type: String },
+    plot: { type: String },
+    posters: { type: String },
+    soundEffects: {
+        type: [{
+            type: String,
+        }]
+    },
+    stills: {
+        type: [{
+            type: String,
+        }]
+    },
+    title: { type: String },
+    imdbId: { type: String },
+    listingType: { type: String },
+    imdbRating: { type: String },
+});
+
+module.exports = mongoose.model('Movies', MoviesSchema);
